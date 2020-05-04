b146dbb8458908c5e05add31461fb5c18676d19f
Andi Irham <Andi.irhamm@gmail.com>
2020-05-04 10:18:44 +0700

    [Andi-IM] init new node webs

---
+++ list-app/app.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}));
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'list_app'
+});
+
+app.get('/', (req, res) => {
+    res.render('top.ejs');
+});
+
+app.get('/index', (req, res) => {
+    connection.query(
+        'SELECT * FROM items',
+        (error, results) => {
+            console.log(results);
+            res.render('index.ejs',{items:results});
+        }
+    );
+});
+
+app.get('/new', (req, res) => {
+    res.render('new.ejs');
+});
+
+app.post('/create', (req, res) => {
+    connection.query(
+        'INSERT INTO items (name) VALUES (?)',
+        [req.body.itemName],
+        (error, results) => {
+          res.redirect('/index');
+        }
+      );
+})
+
+connection.connect((err) => {
+    if (err) {
+        console.log('error connecting: '+ err.stack);
+        return;
+    }
+    console.log('success');
+});
+
+app.get('/hello', (req, res) => {
+    res.render('hello.ejs');
+});
+
+
+
+app.listen(4000);
